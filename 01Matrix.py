25d48781939d34ccf09e14e250abbfdac31f53b3
Zheng Yufei <2415571409@qq.com>
2020-01-19 10:47:46 +0800

    New solutions

--- 01Matrix.py
+++ 01Matrix.py
@@ -1,23 +1,23 @@
 def updateMatrix(matrix):
     from collections import deque
     queue = deque()
-    dist = [[0 for i  in range(matrix[0])]for _ in range(matrix)]
-    for i in range(matrix):
-        for j in range(matrix[i]):
+    dist = [[0 for i in range(len(matrix[0]))] for _ in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             if matrix[i][j] == 0:
                 dist[i][j] = 0
                 queue.appendleft((i, j))
             else:
                 dist[i][j] = 9000
-    dir[4][2] = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    while not queue:
+    dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    while queue:
         x, y = queue[-1]
         queue.pop()
         for i in range(4):
-            new_x, new_y = x+dir[i][0], x + dir[i][1]
-            if new_x >=0 and new_x<4 and new_y>=0 and new_y<4:
+            new_x, new_y = x+dir[i][0], y + dir[i][1]
+            if new_x >=0 and new_x<len(matrix) and new_y>=0 and new_y<len(matrix[0]):
                 if dist[new_x][new_y] > dist[x][y] +1:
                     dist[new_x][new_y] = dist[x][y]+1
                     queue.appendleft((new_x,new_y))
     return dist
-
+print(updateMatrix([[0,0,0], [0,0,0], [0,0,0]]))
